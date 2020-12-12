793bf250b114276c773ee14be746aabcc32c02ca
Dheeraj Tiwari <tdheeraj9a@gmail.com>
2020-12-12 22:05:53 +0530

    Day-1

---
+++ Day-1/function.py
@@ -0,0 +1,6 @@
+def fun():
+    """this is the description about the functon"""
+    print('hey how are you')
+
+
+print(fun())  # If we are not writing return statement then default return value is None
