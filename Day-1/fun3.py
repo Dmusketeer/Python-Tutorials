332e1eb05a16c3398580a6fe283f7719931213f8
Dheeraj Tiwari <tdheeraj9a@gmail.com>
2020-12-12 22:04:48 +0530

    fun3

---
+++ Day-1/fun3.py
@@ -0,0 +1,9 @@
+# Write a Function to check whether the given Number is Even OR Odd?
+def evenOdd(num):
+    if num%2==0:
+        return 'even'
+    else:
+        return 'odd'
+
+print(evenOdd(3))   
+print(evenOdd(10))
