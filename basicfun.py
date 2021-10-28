47eb2dc2454a907eb2684847249540581838d67b
Dmusketeer <ardmountronics@gmail.com>
2021-10-28 12:07:01 +0530

    basic fun

--- basicfun.py
+++ basicfun.py
@@ -1,5 +1,13 @@
 def fun():
-    print(a)
+    print(a);
 
 a=[1,3,4,5]
 fun()
+
+
+
+def fullname(firstname, middlename, lastname):  # Concatenate Strings
+    fullname = f"{firstname} {middlename} {lastname}";
+    print(fullname)
+
+fullname('Asif', 'Ali', 'Bhat');
