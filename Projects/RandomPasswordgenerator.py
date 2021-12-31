9adb6b9c2c4b8da3c4f976f3ff22072f30ccf272
Dmuskeeter <tdheeraj9a@gmail.com>
2021-12-31 14:58:23 +0530

    random password generator

--- Projects/RandomPasswordgenerator.py
+++ Projects/RandomPasswordgenerator.py
@@ -1,10 +1,23 @@
+# importing required modeules
 import random
-print("  Password Generator")
-print("======================")
-# numOfPassword=int(input("Number of Password ? "))
-# passwordLength=int(input("Password Length ? "))
+import string
 
-print(random.randint(1,11))
+print("                                     Password Generator                " )
+print("                                   ======================                     ")
+numOfPassword=int(input("Number of Password ? "))
+passwordLength=int(input("Password Length ? "))
+# string.ascii_letters
+
+lower_case=string.ascii_lowercase
+upper_case=string.ascii_uppercase
+digits=string.digits
+special_char=string.punctuation
+all_char=lower_case+upper_case+digits+special_char
+# print(all_char)
+for i in range(numOfPassword):
+    temp=random.sample(all_char,passwordLength)
+    password="".join(temp)
+    print(password)
 
 
 
