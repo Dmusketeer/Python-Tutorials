45b48e42ba9332f09856c21d7560ccec513d5a07
Dmuskeeter <tdheeraj9a@gmail.com>
2021-12-31 14:20:36 +0530

    modified

--- Day_1/fileSys.py
+++ Day_1/fileSys.py
@@ -1,6 +1,8 @@
 # read contents of file at any locations
 import os
 print(os.getcwd())
+# \\ for connecting the path
 file=open(r'e:\\Git_Repos\\Python\\Day_1\\test.txt')
 print(file.read())
+file.close()
 
---
+++ Projects/RandomPasswordgenerator.py
@@ -0,0 +1,10 @@
+import random
+print("  Password Generator")
+print("======================")
+# numOfPassword=int(input("Number of Password ? "))
+# passwordLength=int(input("Password Length ? "))
+
+print(random.randint(1,11))
+
+
+
