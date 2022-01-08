5b6c8a5487560d170532fd8736117aff379b75da
Dmuskeeter <tdheeraj9a@gmail.com>
2022-01-08 18:56:52 +0530

    assert keyword

---
+++ Random/assetKeyword.py
@@ -0,0 +1,16 @@
+# The assert keyword lets you test if a condition in your code returns True, if not, the 
+# program will raise an AssertionError than can be written by ourselves.
+
+
+# Syntax 
+# assert {condition}, {message}
+
+
+name = "Dheeraj"
+#if condition returns False, AssertionError is raised:
+assert name == "dheeraj", "x should be 'Dheeraj'"
+
+
+
+
+
