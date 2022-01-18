dbca099dcb1de6b12247d9a9f893a7379ef1aaeb
Dmuskeeter <tdheeraj9a@gmail.com>
2022-01-18 08:29:09 +0530

    another file

---
+++ Random/module/anotherMethod.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+
+# NOTE : When importing using the from keyword, do not use the module name when referring to elements in the module
+
+from myModule import Name,Greeting
+
+Name("Dheeraj")
+Name("Jorey")
+
+print(Greeting())
