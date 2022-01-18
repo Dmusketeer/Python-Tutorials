91c5fa8de4f96dd31ec6e63714e6096b1ac92834
Dmuskeeter <tdheeraj9a@gmail.com>
2022-01-18 08:28:13 +0530

    file where we'll import module

---
+++ Random/module/main.py
@@ -0,0 +1,11 @@
+import myModule
+# OR
+# import myModule as MM
+
+myModule.Name("Dheeraj") # or MM.Name("Dheeraj")
+myModule.Name("Jonah") # or MM.Name("Jonah")
+
+print(myModule.Greeting())
+# print(MM.Greeting())
+
+
