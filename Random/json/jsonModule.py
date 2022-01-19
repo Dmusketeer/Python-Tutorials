8f024ceac4c022f9d87524e378677c6c311679d6
Dmuskeeter <tdheeraj9a@gmail.com>
2022-01-19 08:28:01 +0530

    json

--- Random/json/jsonModule.py
+++ Random/json/jsonModule.py
@@ -53,3 +53,27 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
+
+
+
+
+
+xx = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# convert into JSON:
+yy = json.dumps(xx)
+
+# the result is a JSON string:
+print(yy)
