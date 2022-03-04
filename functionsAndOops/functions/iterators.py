545f6e7ecf584968d3c239bdc57d2b427e3813e6
Dmuskeeter <tdheeraj9a@gmail.com>
2022-03-04 16:14:50 +0530

    iterators in python

---
+++ functionsAndOops/functions/iterators.py
@@ -0,0 +1,10 @@
+#                               Iterators
+
+
+x=[1,2,3,4]
+s=iter(x)
+print(next(s)) #1
+print(next(s)) #2
+print(next(s)) #3
+print(next(s)) #4
+print(next(s)) #StopIteration Error
