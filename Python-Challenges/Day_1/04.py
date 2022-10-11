549a33526ee82032267651353c2512e1f610b818
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-11 20:36:08 +0530

    taking two integer input from users

---
+++ Python-Challenges/Day_1/04.py
@@ -0,0 +1,11 @@
+# Ask the user to enter 
+# two numbers. Add 
+# them together and 
+# display the answer as 
+# The total is 
+# [answer].
+
+
+
+num1,num2=map(int,input("Enter two numbers :").split())
+print(f"The Total is {num1+num2}") 
