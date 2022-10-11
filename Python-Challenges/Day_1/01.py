6ca0ae4ec7ffcbb9aad59cf224131f0561ca6d9c
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-11 20:24:29 +0530

    basic inputs taking from users

---
+++ Python-Challenges/Day_1/01.py
@@ -0,0 +1,5 @@
+# Ask for the user’s first name and display the output message. 
+# Hello [First Name] 
+
+Name=input("Enter your name:")
+print(f'Hello {Name}')
---
+++ Python-Challenges/Day_1/02.py
@@ -0,0 +1,8 @@
+# Ask for the user’s first name and then ask for 
+# their surname and display the output message 
+# Hello [First Name] [Surname]. 
+
+first_Name=input("please enter first name :")
+sur_Name=input("please enter Surname :")
+
+print(f"Hello {first_Name} {sur_Name}")
