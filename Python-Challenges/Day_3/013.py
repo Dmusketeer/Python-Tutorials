b4671794cf25cc2636d0c22cf814f0845ad30f76
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-15 21:36:09 +0530

    conditional statements

---
+++ Python-Challenges/Day_3/013.py
@@ -0,0 +1,14 @@
+# Ask the user to enter a 
+# number that is under 
+# 20. If they enter a 
+# number that is 20 or 
+# more, display the 
+# message “Too high”, 
+# otherwise display 
+# “Thank you”. 
+
+Number=int(input("Enter a nummber under 20: "))
+if Number == 20 or Number > 20:
+    print("Too High")
+else:
+    print("Thank You")
