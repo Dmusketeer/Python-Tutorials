a902bc1f1144784357133ec0e790621238cc8bbf
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-18 08:20:10 +0530

    maths module in python

---
+++ Python-Challenges/Day_6/022.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a 
+# number with lots of 
+# decimal places. Multiply 
+# this number by two and 
+# display the answer.display the answer to 
+# two decimal places.
+# 
+
+userInput=float(input("Enter a Number with lots of decimal places : "))
+print(round(userInput*2,2))
