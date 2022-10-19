814100a1373a0c4ccf1ea15224e02e0e3d38a903
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-19 08:23:10 +0530

    math module

---
+++ Python-Challenges/Day_6/023.py
@@ -0,0 +1,6 @@
+# Ask the user to enter an integer that is over 500. Work 
+# out the square root of that number and display it to two
+# decimal places. 
+import math
+UserInput=int(input("Enter an integer that is over 500: "))
+print(round(math.sqrt(UserInput),2))
