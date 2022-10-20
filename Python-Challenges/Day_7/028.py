fe82ba17fbc8ed279e9049587377100146d0bced
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-20 23:29:52 +0530

    loops

---
+++ Python-Challenges/Day_7/028.py
@@ -0,0 +1,10 @@
+# Ask the user to enter 
+# their name and then 
+# display their name 
+# and a number and then display their name that 
+# number of times. 
+
+userName=input("Enter Your Name: ")
+num=int(input("Enter numbers "))
+for i in range(num):
+    print(userName)
