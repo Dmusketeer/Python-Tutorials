a8565e98ad1d16bdc568782de98586ff58c43f47
Dmuskeeter <tdheeraj9a@gmail.com>
2022-10-30 09:46:57 +0530

    draw square using turtle module

---
+++ Python-Challenges/Day_10/044.py
@@ -0,0 +1,16 @@
+# Draw a Square
+
+import turtle
+# turtle.color("green","red")
+# turtle.shape("turtle")
+# turtle.forward(100)
+# turtle.left(120)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+turtle.shape("turtle")
+for i in range(0,4):
+    turtle.forward(100)
+    turtle.left(90)
+turtle.exitonclick()
