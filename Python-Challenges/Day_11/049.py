2a105f2e8c8f26254f972bcf830c264df29f7fce
Dmuskeeter <tdheeraj9a@gmail.com>
2022-11-01 16:58:13 +0530

    list operations

---
+++ Python-Challenges/Day_11/049.py
@@ -0,0 +1,9 @@
+# Create a list of two sports. Ask the 
+# user what their favourite sport is and 
+# add this to the end of the list. Sort the 
+# list and display it.
+
+sports=["cricket","football"]
+sports.append(input("Enter your fevorite sport: "))
+sports.sort()
+print(sports)
