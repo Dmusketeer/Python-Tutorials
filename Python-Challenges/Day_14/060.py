801b2ad27e47f6f8795cd4b9a4f2d5c6487f404c
Dmuskeeter <tdheeraj9a@gmail.com>
2022-12-05 07:50:09 +0530

    file opening and writing

---
+++ Python-Challenges/Day_14/060.py
@@ -0,0 +1,20 @@
+# Write a new file called 
+# “Numbers.txt”. Add five numbers 
+# to the document which are stored 
+# on the same line and only 
+# separated by a comma. Once you 
+# have run the program, look in 
+# the location where your program is stored and you 
+# should see that the file has been 
+# created. The easiest way to 
+# view the contents of the new text file
+# on a Windows system is to read it using Notepad. 
+
+
+numbers_file=open("numbers.txt","w")
+numbers_file.write("1,")
+numbers_file.write("2,")
+numbers_file.write("3,")
+numbers_file.write("4,")
+numbers_file.write("5,")
+numbers_file.close()
