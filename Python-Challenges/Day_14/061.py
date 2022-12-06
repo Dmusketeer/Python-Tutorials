912f2d283a3bd7f4e5a1d7ebd45a4ae78269aa82
Dmuskeeter <tdheeraj9a@gmail.com>
2022-12-06 19:14:51 +0530

    File system in python

--- Python-Challenges/Day_14/061.py
+++ Python-Challenges/Day_14/061.py
@@ -10,3 +10,35 @@
 # with a new file. If they select 2, display the contents of the 
 # “Subject.txt” file. If they select 3, ask the user to enter a new 
 # subject and save it to the file and then display the entire contents of the file.
+
+
+
+print("1) Create a new file.")
+print("2) Display the file.")
+print("3) Add a new item to the file.")
+print("Make a selection 1,2 or 3.")
+
+userChoice=int(input("Enter 1, 2 or 3 : "))
+
+if userChoice==1 or userChoice==2 or userChoice==3:
+    if userChoice==1:
+        subject=input("Enter school subject Names: ")
+        file=open("Day_14/Subject.txt","w")
+        file.write(subject)
+        file.close()
+    elif userChoice==2:
+        file=open("Day_14/Subject.txt","r")
+        print(file.read())
+        file.close()
+    else:
+        pass
+        newSubject=input("Enter New subject Names: ")
+        file=open("Day_14/Subject.txt","a")
+        file.write(newSubject)
+        file.close()
+        file=open("Day_14/Subject.txt","r")
+        print(file.read())
+        file.close()
+else:
+    raise Exception("Enter the Correct Number.")
+
